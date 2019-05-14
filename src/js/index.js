d0db320f6ea8782c5b01d35907349049d7b579f8
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-13 21:34:37 -0500

    Fixed bad formatting in .babelrc file, added babel-polyfill entry point to webpack config file, changed babel-loader to version 7 due to compatibility issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num from "./test";
-
-console.log(`I impossssssrted ${num} from another module`);
+const x = 23;
+console.log(`I impossssssrted ${num} from another module. Variable x is ${x}`);
