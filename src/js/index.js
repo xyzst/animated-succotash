bc2edb6947bd1b5378b2b5bb11309ef8a66c820e
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-14 21:14:41 -0500

    Stubbed out controller method for search results

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,36 @@
 // Global app controller
 import Search from "./models/Search";
 
+/**
+ * Global state of app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get query from view
+  const query = "pizza"; // todo!
+
+  if (query) {
+    // 2. new search object and add to state
+    state.search = new Search(query);
+
+    // 3. prepare UI for results
+
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    console.log(state.search.result);
+  }
+};
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault(); // prevent page refresh on form submit
+  controlSearch();
+});
+
 const search = new Search("pizza");
