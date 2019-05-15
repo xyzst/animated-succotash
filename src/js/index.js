191ef044ee68e0a3001148c317c88ec42153ee25
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-14 20:23:20 -0500

    Learning about named exports

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import string from "./models/Search";
-import { add, multiply, ID } from "./views/searchView";
+import { add as a, multiply as m, ID } from "./views/searchView";
 
 console.log(string);
-console.log(add(1, 1));
-console.log(multiply(ID, 5));
+console.log(a(1, 1));
+console.log(m(ID, 5));
