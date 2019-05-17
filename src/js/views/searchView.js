6570ae7e4c9965d0c4f8d37eaf042f7aee065b76
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-16 20:55:59 -0500

    Add helper method to shorten the length of each recipe title when rendering the recipes

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -29,11 +29,48 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
     </li>
     `;
   elements.searchResultList.insertAdjacentHTML("beforeend", markup);
+};
+
+/**
+ * Shortens strings given in the title parameter by the defined limit parameter (default is 17)
+ *
+ * Example
+ *  Given:
+ *      title: Pasta with tomato and spinach",
+ *      limit: 17
+ *
+ *  Then:
+ *      begin...
+ *          0th iteration: accumulator (0) + current.length = 5 / newTitle = ['Pasta']
+ *          1st iteration: accumulator (5) + current.length = 9 / newTitle = ['Pasta', 'with']
+ *          2nd iteration: accumulator (9) + current.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+ *          3rd iteration: accumulator (15) + current.length = 18 / newTitle = ['Pasta', 'with', 'tomato'] // No longer adding words
+ *          4th iteration: accumulator (18) + current.length = 25 / newTitle = ['Pasta', 'with', 'tomato']
+ *      ...done
+ *
+ * @param {string} title
+ * @param {number} limit
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    let words = title.split(" ");
+    words.reduce((accumulator, current) => {
+      if (accumulator + current.length <= limit) {
+        newTitle.push(current);
+      }
+      return accumulator + current.length;
+    }, 0);
+
+    return `${newTitle.join(" ")} ...`;
+  }
+
+  return title;
 };
