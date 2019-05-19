c2f4259d9c0887e0f1a62da1eda1e4fa1c4e6eb2
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-18 22:32:16 -0500

    Render recipe view and leverage new recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 /**
  * Global state of app
@@ -64,7 +65,8 @@ const controlRecipe = async () => {
   const id = parseInt(window.location.hash.replace("#", ""));
   if (id) {
     // prepare ui
-
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     // create new recipe object
     state.recipe = new Recipe(id);
     // get recipe data  & parse ingredients
@@ -74,6 +76,9 @@ const controlRecipe = async () => {
       // calculate servers and time
       state.recipe.calcTime();
       state.recipe.calcServings();
+
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (error) {
       alert("Error processing recipe!");
     }
