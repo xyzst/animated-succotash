aa81f7badf44e0a7b8dd6a41af37b11b69487970
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-18 22:56:55 -0500

    Add format method to display mixed numbers appropriately

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,6 @@
 import { elements } from "./base";
+import { Fraction } from "fractional";
+
 export const clearRecipe = () => {
   elements.recipe.innerHTML = "";
 };
@@ -7,7 +9,7 @@ export const createIngredient = ingredient => `
     <svg class="recipe__icon">
         <use href="img/icons.svg#icon-check"></use>
     </svg>
-    <div class="recipe__count">${ingredient.count}</div>
+    <div class="recipe__count">${this.formatCount(ingredient.count)}</div>
     <div class="recipe__ingredient">
         <span class="recipe__unit">${ingredient.unit}</span>
         ${ingredient.ingredient}
@@ -99,4 +101,26 @@ export const renderRecipe = recipe => {
     `;
 
   elements.recipe.insertAdjacentHTML("afterbegin", markup);
+};
+
+const formatCount = count => {
+  if (count) {
+    // count = 2.5 ->> 2 1/2
+    // count = 0.5 ->> 1/2
+    const [int, dec] = count
+      .toString()
+      .split(".")
+      .map(x => parseInt(x, 10));
+
+    if (!dec) return count;
+
+    if (int === 0) {
+      const fr = new Fraction(count);
+      return `${fr.numerator}/${fr.denominator}`;
+    } else {
+      const fr = new Fraction(count - int);
+      return `${int} ${fr.numerator}/${fr.denominator}`;
+    }
+  }
+  return "?";
 };
