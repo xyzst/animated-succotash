51b413d75a7f814fb0cc503dd2eb3578775f858f
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-19 14:51:11 -0500

    Add Likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,30 @@
+export default class Likes {
+  constructor() {
+    this.likes = [];
+  }
+
+  addLike(id, title, author, image) {
+    const like = {
+      id,
+      title,
+      author,
+      image
+    };
+
+    this.likes.push(like);
+    return like;
+  }
+
+  removeLike(id) {
+    const index = this.likes.findIndex(el => el.id === id);
+    this.likes.splice(index, 1);
+  }
+
+  isLiked(id) {
+    return this.likes.findIndex(el => el.id === id) !== -1;
+  }
+
+  getNumLike() {
+    return this.likes.length;
+  }
+}
