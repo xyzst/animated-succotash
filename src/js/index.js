9c6a66abda9edd74d837c5c1d63ed38ce5f78838
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-19 14:13:08 -0500

    Add new dependency to create unique identifiers for each shopping item, add import to controller file, update package.json

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
